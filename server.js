0038bfe09e10edf74119c9d64e918a448471f1ac
Jonny Lightheart <jonnylightheart@gmail.com>
2020-10-28 11:46:16 -0600

    added inital back end and front end framework

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = process.env.PORT || 3010;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${[PORT]}`)
+});
